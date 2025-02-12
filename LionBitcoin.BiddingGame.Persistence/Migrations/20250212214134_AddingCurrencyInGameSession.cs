e6bd59b3fb2ce141d06aaebfe7b594e11eb078b8
SandroKavlashvili <kavlashvilisandro1@gmail.com>
2025-02-13 01:42:31 +0400

    adding currency in game sessions data

---
+++ LionBitcoin.BiddingGame.Persistence/Migrations/20250212214134_AddingCurrencyInGameSession.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LionBitcoin.BiddingGame.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingCurrencyInGameSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "currency",
+                table: "game_sessions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "currency",
+                table: "game_sessions");
+        }
+    }
+}
