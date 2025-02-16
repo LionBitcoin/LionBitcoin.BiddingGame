55ca0ac02bd807904c99a793f25db28044993fd1
SandroKavlashvili <kavlashvilisandro1@gmail.com>
2025-02-16 22:08:31 +0400

    switch on fire and forget

--- LionBitcoin.BiddingGame.Application/Features/Game/PreProcessGameSessions/PreProcessGameSessionsCommandHandler.cs
+++ LionBitcoin.BiddingGame.Application/Features/Game/PreProcessGameSessions/PreProcessGameSessionsCommandHandler.cs
@@ -29,7 +29,9 @@ public class PreProcessGameSessionsCommandHandler : IRequestHandler<PreProcessGa
             CancellationToken = cancellationToken
         };
 
-        await Parallel.ForEachAsync(
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        Parallel.ForEachAsync( // Fire and forget PreProcess method for game sessions.
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             gameSessions,
             options,
             PreProcessGameSession);
